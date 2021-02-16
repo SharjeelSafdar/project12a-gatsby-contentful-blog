6952a632aeb01a13819864e993d5b7685e0f04fe
Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-02-16 14:10:28 +0500

    Added TypeScript support in gatsby-config and gatsby-node

---
+++ src/gatsby/config.ts
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+export default {
+  siteMetadata: {
+    title: `Gatsby Contentful Blogsite`,
+    description: `A blogsite with Gatsby, Contentful, Netlify, Firebase and Typescript.`,
+    author: {
+      name: `MianMuhammadSharjeelSafdar`,
+      github: `https://github.com/SharjeelSafdar`,
+    },
+    siteUrl: `https://gatsby-blog-p12a.netlify.app/`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Contentful Blogsite`,
+        short_name: `Gatsby Blog`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
