9b9056ab35d0fad0b71ae633cd8b8988bb090e92
Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-02-18 05:35:29 +0500

    Updated home to include info about blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,13 @@ const IndexPage: FC = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to our Gatsby blog site.</p>
+    <Link to="/blog">
+      <p>Visit our blog page.</p>
+    </Link>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 );
 
