6930488e672c57412b84598a4d7ad0a1c7b35952
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-02-25 03:23:16 +0500

    Added info of logged in user in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,35 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
+import { useAuth } from "../context/authContext";
 
-const IndexPage: FC = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to our blogsite with Gatsby and Contentful.</p>
-    <Link to="/blog">
-      <p>Visit our blog page.</p>
-    </Link>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-);
+const IndexPage: FC = () => {
+  const { isSignedIn, userNameOrEmail } = useAuth();
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to our blogsite with Gatsby and Contentful.</p>
+      <p>
+        You are currently {""}
+        {isSignedIn ? (
+          <span>
+            logged in as <strong>{userNameOrEmail}</strong>
+          </span>
+        ) : (
+          <span>not logged in</span>
+        )}
+        .
+      </p>
+      <Link to="/blog">
+        <p>Visit our blog page.</p>
+      </Link>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
